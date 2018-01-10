dde1ded151085e1c3663b6d2c999115a3b262889
XuChengHUST <31894585+XuChengHUST@users.noreply.github.com>
2018-01-10 19:06:05 +0800

    Create common_include.h

---
+++ include/common_include.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_INCLUDE_H
+#define COMMON_INCLUDE_H
+
+// c++ stl
+#include <memory>
+#include <string>
+#include <vector>
+#include <iostream>
+
+// for Eigen
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#endif
