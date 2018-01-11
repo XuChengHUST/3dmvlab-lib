8a79bae801c2ff1c1a6f226ed17836750ac535aa
XuChengHUST <xuchenghust@gmail.com>
2018-01-12 04:00:53 +0800

    all algo should be in pc namespace

--- include/remove_outliers.h
+++ include/remove_outliers.h
@@ -3,4 +3,7 @@
 #include "kdtree/kdtree_flann.h"
 
 //输出去孤点后的点云序号序号
-void remove_outliers(pc::PointCloud& cloud, std::vector<int>& out, float a, int k);
+namespace pc {
+  void remove_outliers(pc::PointCloud& cloud, std::vector<int>& out,
+                       int k = 10, float factor = 1.0f);
+}
