6d321a8a4bc654a348f327e899d3d6df3a7dff4c
root <allensnawu@gmail.com>
2018-01-12 10:33:25 +0800

    update

--- include/remove_outliers.h
+++ include/remove_outliers.h
@@ -1,6 +1,11 @@
+#ifndef REMOVE_OUTLIERS_H
+#define REMOVE_OUTLIERS_H
+
 #include "point_cloud.h"
 #include "common_include.h"
 #include "kdtree/kdtree_flann.h"
 
 //输出去孤点后的点云序号序号
 void remove_outliers(pc::PointCloud& cloud, std::vector<int>& out, float a, int k);
+
+#endif
