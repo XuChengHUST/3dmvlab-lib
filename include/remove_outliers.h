d3620a7d611e8ffca4558865e274efe836c22231
XuChengHUST <xuchenghust@gmail.com>
2018-01-13 16:21:43 +0800

    add namespace define

--- include/remove_outliers.h
+++ include/remove_outliers.h
@@ -1,9 +1,13 @@
+#ifndef REMOVE_OUTLIERS_H
+#define REMOVE_OUTLIERS_H
+
 #include "point_cloud.h"
 #include "common_include.h"
 #include "kdtree/kdtree_flann.h"
 
-//输出去孤点后的点云序号序号
 namespace pc {
   void remove_outliers(pc::PointCloud& cloud, std::vector<int>& out,
                        int k = 10, float factor = 1.0f);
 }
+
+#endif
