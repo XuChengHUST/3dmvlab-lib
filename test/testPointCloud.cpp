65176d3613be542313ee1158f55392446e3cedb3
root <allensnawu@gmail.com>
2018-01-13 21:03:24 +0800

    update

--- test/testPointCloud.cpp
+++ test/testPointCloud.cpp
@@ -1,10 +1,17 @@
 #include "point_cloud.h"
+#include "io/io.h"
 
 int main(int argc, char** argv)
 {
-  Eigen::MatrixX3f matrix;
-  matrix.resize(10,3);
-  pc::PointCloud pc(matrix);
-  std::cout << "pts size:" << pc.size() << '\n'
-            << pc;
+  // Eigen::MatrixX3f matrix;
+  // matrix.resize(10,3);
+  // pc::PointCloud pc(matrix);
+  // std::cout << "pts size:" << pc.size() << '\n'
+  //           << pc;
+
+  pc::PointCloud pts;
+  // pc::readACToPointCloud(argv[1], pts);
+  pc::ReadASC_xyz(argv[1], pts);
+  std::cout << "pts size:" << pts.size() << '\n'
+            << pts;
 }
