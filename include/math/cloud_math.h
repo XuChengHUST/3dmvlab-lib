077a67008da820e06d5fb107b0c79740d3f575b0
root <allensnawu@gmail.com>
2018-01-13 22:07:13 +0800

    update

--- include/math/cloud_math.h
+++ include/math/cloud_math.h
@@ -12,7 +12,7 @@ namespace pc {
   template <typename T>
   float mean(const std::vector<T>& v_set) {
     float sum = std::accumulate(v_set.begin(), v_set.end(), 0.0f);
-    return (sum/static_cast<int>(v.size()));
+    return (sum/static_cast<int>(v_set.size()));
   }
   // float standard_deviation(const std::vector<float>& v) {
   //   float sqr_sum = std::inner_product(v.begin(), v.end(), v.begin(), 0.0f);
@@ -24,7 +24,7 @@ namespace pc {
   float standard_deviation(const std::vector<T>& v_set) {
     float sqr_sum = std::inner_product(v_set.begin(), v_set.end(), v_set.begin(), 0.0f);
     float mean_value = mean(v_set);
-    return std::sqrt(sqr_sum/static_cast<int>(v.size()) - mean_value * mean_value);
+    return std::sqrt(sqr_sum/static_cast<int>(v_set.size()) - mean_value * mean_value);
   }
 }
 
