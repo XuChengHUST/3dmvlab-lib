04ca182ddc7ea4447e9be913598769b4fb4b3819
XuChengHUST <xuchenghust@gmail.com>
2018-01-14 14:27:55 +0800

    move def to source file

--- include/math/cloud_math.h
+++ include/math/cloud_math.h
@@ -6,16 +6,9 @@
 #include <cmath>
 
 namespace pc {
-  float mean(const std::vector<float>& v) {
-    float sum = std::accumulate(v.begin(), v.end(), 0.0f);
-    return (sum/static_cast<int>(v.size()));
-  }
+  float mean(const std::vector<float>& v);
 
-  float standard_deviation(const std::vector<float>& v) {
-    float sqr_sum = std::inner_product(v.begin(), v.end(), v.begin(), 0.0f);
-    float mean_value = mean(v);
-    return std::sqrt(sqr_sum/static_cast<int>(v.size()) - mean_value * mean_value);
-  }
+  float standard_deviation(const std::vector<float>& v);
 }
 
 #endif
