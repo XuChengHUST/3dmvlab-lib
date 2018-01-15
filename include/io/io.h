fe887f0f19660698062fe473a17a2468df5f778a
root <allensnawu@gmail.com>
2018-01-15 13:35:53 +0800

    add readASCToPointCloud

--- include/io/io.h
+++ include/io/io.h
@@ -4,9 +4,11 @@
 #include "common_include.h"
 #include "io/ac_type.h"
 #include "point_cloud.h"
+#include <fstream>
 
 namespace pc {
   void readACToPointCloud(const std::string& filename, pc::PointCloud& pc);
+  void readASCToPointCloud(const std::string& filename, pc::PointCloud& pc);
   // void writePCFileAscii(const std::string& filename, std::vector<int>& point);
 }
 
--- src/io/io.cpp
+++ src/io/io.cpp
@@ -14,22 +14,25 @@ namespace pc {
                                    ac.pVertex_[3*i+2]));
     }
   }
-  // void writePCFileAscii(const std::string& filename, pc::PointCloud& pc)
-  // {
-  //   std::ofstream ofs;
-  //   ofs.open(filename);
-  //   if(!ofs.is_open())
-  //   {
-  //     std::cout << filename << " open failed." << '\n';
-  //     return;
-  //   }
-  //   else
-  //   {
-  //     for(int i = 0; i != pts.size(); ++i)
-  //       ofs << pc.at(i).x << " "
-  //           << pc.at(i).y << " "
-  //           << pc.at(i).z << '\n';
-  //   }
-  //   ofs.close();
-  // }
+  void readASCToPointCloud(const std::string& filename, pc::PointCloud& pc) {
+    std::ifstream inFile;
+    std::ifstream fin;
+    int txt_size = 0;
+    double x, y, z;
+    inFile.open(filename.c_str());
+    while(!inFile.eof())
+    {
+      inFile >> x >> y >> z;
+      txt_size++;
+    }
+    inFile.close();
+
+    fin.open(filename.c_str());
+    for(size_t i=0;i != txt_size;++i)
+    {
+      fin >> x >> y >> z;
+      pc.push_back(pc::PointNormal(x,y,z) );
+    }
+    fin.close();
+  }
 }
