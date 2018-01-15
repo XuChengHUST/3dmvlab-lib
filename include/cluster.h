67244abe2a88ed9f27287a29a53c372ac864c656
XuChengHUST <xuchenghust@gmail.com>
2018-01-15 13:44:13 +0800

    remove unused

--- include/cluster.h
+++ include/cluster.h
@@ -6,11 +6,6 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
-<<<<<<< HEAD
-#include <vector>
-=======
-#include <string>
->>>>>>> 739014921de787cca733afe567fab21d9a910460
 
 namespace pc{
   void cluster(pc::PointCloud& cloud, std::vector<std::vector<int> >& cluster_indices, int k_close, float distance_threshold);
